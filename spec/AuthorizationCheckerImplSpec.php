564b74fe8b55f489c4e0a09ea1ff035e48a9ba6d
Dawid Nowak <code@dnowak.pl>
2016-12-29 23:12:24 +0100

    AuthorizationChecker

---
+++ spec/AuthorizationCheckerImplSpec.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Dvdnwk\BehatExample;
+
+use Dvdnwk\BehatExample\AuthorizationChecker;
+use Dvdnwk\BehatExample\AuthorizationCheckerImpl;
+use Dvdnwk\BehatExample\User;
+use PhpSpec\ObjectBehavior;
+
+class AuthorizationCheckerImplSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith(null);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(AuthorizationCheckerImpl::class);
+    }
+
+    function it_is_authorization_checker()
+    {
+        $this->shouldHaveType(AuthorizationChecker::class);
+    }
+
+    public function it_grants_user_role_for_a_user()
+    {
+        $user = new User('');
+
+        $this->beConstructedWith($user);
+
+        $this->isGranted('user')
+            ->shouldReturn(true);
+    }
+
+    public function it_does_not_grant_user_role_when_no_user()
+    {
+        $this->isGranted('user')
+            ->shouldReturn(false);
+    }
+
+    public function it_grants_admin_role_when_user_is_admin()
+    {
+        $user = new User('', true);
+
+        $this->beConstructedWith($user);
+
+        $this->isGranted('admin')
+            ->shouldReturn(true);
+    }
+
+    public function ir_does_not_grant_admin_role_when_no_user()
+    {
+        $this->isGranted('admin')
+            ->shouldReturn(false);
+    }
+
+    public function it_does_not_grant_admin_role_when_user_is_not_admin()
+    {
+        $user = new User('');
+
+        $this->beConstructedWith($user);
+
+        $this->isGranted('admin')
+            ->shouldReturn(false);
+    }
+
+    public function it_does_not_grant_unknown_roles_even_for_admins()
+    {
+        $user = new User('', true);
+
+        $this->beConstructedWith($user);
+
+        $this->isGranted('foo')
+            ->shouldReturn(false);
+    }
+}
---
+++ src/AuthorizationCheckerImpl.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dvdnwk\BehatExample;
+
+class AuthorizationCheckerImpl implements AuthorizationChecker
+{
+    /**
+     * @var User|null
+     */
+    private $user;
+
+    public function __construct(?User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function isGranted(string $role): bool
+    {
+        if (null === $this->user) {
+            return false;
+        }
+
+        switch ($role) {
+            case 'user':
+                return true;
+            case 'admin':
+                return $this->user->isAdmin();
+            default:
+                return false;
+
+        }
+    }
+}
