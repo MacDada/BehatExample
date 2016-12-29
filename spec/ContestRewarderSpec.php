3b9758dfd0203e15768c3ed8550d0fd5cb74e1fb
Dawid Nowak <code@dnowak.pl>
2016-12-29 22:09:18 +0100

    ContestRewarder: gives prizes (but does not use a place repository yet)

--- spec/ContestRewarderSpec.php
+++ spec/ContestRewarderSpec.php
@@ -5,12 +5,68 @@ declare(strict_types=1);
 namespace spec\Dvdnwk\BehatExample;
 
 use Dvdnwk\BehatExample\ContestRewarder;
+use Dvdnwk\BehatExample\PlaceAsAwardAssigner;
+use Dvdnwk\BehatExample\User;
+use Dvdnwk\BehatExample\UserRepository;
 use PhpSpec\ObjectBehavior;
+use PhpSpec\Wrapper\Collaborator;
+use Webmozart\Assert\Assert;
 
 class ContestRewarderSpec extends ObjectBehavior
 {
+    function let(UserRepository $userRepository)
+    {
+        $this->beConstructedWith($userRepository, new PlaceAsAwardAssigner());
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType(ContestRewarder::class);
+    }
+
+    function it_returns_no_places_when_none_were_given()
+    {
+        $this->getUsersPlaces()->shouldReturn([]);
+    }
+
+    function it_can_assign_places_to_users()
+    {
+        $this->assignPlaceToUser('John', 1);
+        $this->assignPlaceToUser('Mark', 2);
+
+        $this->getUsersPlaces()->shouldReturn([
+            'John' => 1,
+            'Mark' => 2,
+        ]);
+    }
+
+    function it_gives_away_prizes_to_users(UserRepository $userRepository)
+    {
+        $john = $this->mockUser($userRepository, 'John');
+        $mark = $this->mockUser($userRepository, 'Mark');
+
+        $this->assignPlaceToUser('John', 1);
+        $this->assignPlaceToUser('Mark', 2);
+
+        $this->giveAwayPrizes();
+
+        Assert::same(['1'], $john->getAwards());
+        Assert::same(['2'], $mark->getAwards());
+    }
+
+    /**
+     * @param UserRepository|Collaborator $userRepository
+     * @param string                      $name
+     * @return User
+     */
+    private function mockUser(Collaborator $userRepository, string $name): User
+    {
+        $user = new User($name);
+
+        $userRepository
+            ->findByName($name)
+            ->willReturn($user);
+
+        return $user;
     }
 }
--- src/ContestRewarder.php
+++ src/ContestRewarder.php
@@ -6,4 +6,45 @@ namespace Dvdnwk\BehatExample;
 
 class ContestRewarder
 {
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * @var UserAwardsAssigner
+     */
+    private $userAwardsAssigner;
+
+    /**
+     * todo: it should be not an array but a repository
+     *
+     * @var array
+     */
+    private $usersPlaces = [];
+
+    public function __construct(UserRepository $userRepository, UserAwardsAssigner $userAwardsAssigner)
+    {
+        $this->userRepository = $userRepository;
+        $this->userAwardsAssigner = $userAwardsAssigner;
+    }
+
+    public function getUsersPlaces(): array
+    {
+        return $this->usersPlaces;
+    }
+
+    public function assignPlaceToUser($userName, $place): void
+    {
+        $this->usersPlaces[$userName] = $place;
+    }
+
+    public function giveAwayPrizes(): void
+    {
+        foreach ($this->usersPlaces as $userName => $place) {
+            $user = $this->userRepository->findByName($userName);
+
+            $this->userAwardsAssigner->assignAward($user, $place);
+        }
+    }
 }
